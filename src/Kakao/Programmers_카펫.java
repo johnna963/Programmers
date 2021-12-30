7dcb347a5d5f9ec54363e012fb0fa45e8d924d46
Na, Jiyeop <johnna947@gmail.com>
2021-12-30 21:05:54 +0900

    Programmers_카펫_12/30

---
+++ src/Kakao/Programmers_카펫.java
@@ -0,0 +1,31 @@
+package Kakao;
+
+public class Programmers_카펫 {
+	static class Carpet  {
+	    public int[] solution(int brown, int yellow) {
+	        int[] answer = new int[2];
+	        int area = brown + yellow;
+	        for(int i = 1; i <= area; i++){
+	            int row = i;
+	            int col = area/row;
+	            if(row > col){
+	                continue;
+	            }
+	            int center = (row - 2) * (col - 2);
+	            if(center == yellow){
+	                answer[0] = col;
+	                answer[1] = row;
+	                return answer;
+	            }
+	        }
+	        return answer;
+	    }
+	}
+	public static void main(String[] args) {
+		Carpet sol = new Carpet();
+		int brown = 10;
+		int yellow = 2;
+		sol.solution(brown, yellow);
+	}
+
+}
