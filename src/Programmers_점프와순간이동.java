b26a797ac049d330e2e82e494afe6b177af3a5bd
Na, Jiyeop <johnna947@gmail.com>
2022-02-25 22:10:39 +0900

    Programmers_점프와순간이동_0225

--- src/Programmers_점프와순간이동.java
+++ src/Programmers_점프와순간이동.java
@@ -1,9 +1,24 @@
-
+import java.util.*;
 public class Programmers_점프와순간이동 {
+	static public class JumpAndTeleport {
+	    public int solution(int n) {
+	        int ans = 0;
+	        while(n!=0) {
+	        	if(n % 2 == 0) {
+	        		n = n/2;
+	        	}
+	        	else {
+	        		n--;
+	        		ans++;
+	        	}
+	        }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	        return ans;
+	    }
 	}
-
+	public static void main(String[] args) {
+		JumpAndTeleport sol = new JumpAndTeleport();
+		int n = 5;
+		sol.solution(n );
+	}
 }
