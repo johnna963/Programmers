740484c6a78f2ac8f24de58c23001f5ea4094511
Na, Jiyeop <johnna947@gmail.com>
2022-04-05 21:46:54 +0900

    Programmers_단어변환_0405

---
+++ src/Programmers_단어변환.java
@@ -0,0 +1,42 @@
+
+public class Programmers_단어변환 {
+	static class WordConversion {
+		static boolean[] visited;;
+		static int answer;
+	    public int solution(String begin, String target, String[] words) {
+	        visited = new boolean[words.length];
+	        dfs(begin, target, words, 0);
+	        return answer;
+	    }
+	    public static void dfs(String begin, String target, String[] words, int cnt) {
+	    	if(begin.equals(target)) {
+	    		answer = cnt;
+	    		return;
+	    	}
+	    	for (int i = 0; i < words.length; i++) {
+				if(visited[i]) {
+					continue;
+				}
+				int k = 0;
+				for (int j = 0; j < begin.length(); j++) {
+					if(begin.charAt(j) == words[i].charAt(j)) {
+						k++;
+					}
+				}
+				if(k == begin.length()-1) {
+					visited[i] = true;
+					dfs(words[i], target, words, cnt + 1);
+					visited[i] = false;
+				}
+			}
+	    }
+	}
+	public static void main(String[] args) {
+		WordConversion sol = new WordConversion();
+		String begin = "hit";
+		String target = "cog";
+		String[] words = {"hot", "dot", "dog", "lot", "log", "cog"};
+		System.out.println(sol.solution(begin, target, words));
+	}
+
+}
