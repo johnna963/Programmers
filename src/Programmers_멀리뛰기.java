8fea259b56422ea472a9865998a7c06f5b6fd686
Na, Jiyeop <johnna947@gmail.com>
2022-05-02 21:35:36 +0900

    Programmers_멀리뛰기_0502

---
+++ src/Programmers_멀리뛰기.java
@@ -0,0 +1,23 @@
+
+public class Programmers_멀리뛰기 {
+	static class LongJump {
+	    public long solution(int n) {
+	        long answer = 0;
+	        long dp[] = new long[2001];
+	        dp[1] = 1;
+	        dp[2] = 2;
+	        for (int i = 3; i <= 2000; i++) {
+				dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+			}
+	        answer = dp[n];
+	        return answer;
+	    }
+	}
+
+	public static void main(String[] args) {
+		LongJump sol = new LongJump();
+		int n = 4;
+		System.out.println(sol.solution(n));
+	}
+
+}
