d1fce5ea1b697d877d12ef3fdb77f171749feefc
Na, Jiyeop <johnna947@gmail.com>
2022-05-31 17:16:42 +0900

    Programmers_숫자의표현

---
+++ src/Programmers_숫자의표현.java
@@ -0,0 +1,13 @@
+
+public class Programmers_숫자의표현 {
+	static class ExpressionNumber{
+	    public int solution(int n) {
+	        int answer = 0;
+	        return answer;
+	    }
+	}
+	public static void main(String[] args) {
+		ExpressionNumber sol = new ExpressionNumber();
+		System.out.println("test");
+	}
+}
